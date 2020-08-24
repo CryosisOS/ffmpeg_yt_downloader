4b5e97b8a972e15e31171b4603f97badcce652fd
PotatoMaster101 <PotatoMaster101@users.noreply.github.com>
2020-08-24 17:15:29 +0800

    Finish main

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,26 @@
 #include <iostream>
 #include "include/argparse.hpp"
 
+void actions(argparse::ArgumentParser&& args) {
+    if (auto url = args.present("-u")) {
+        std::cout << "URL stuff" << std::endl;
+    }
+}
+
 int main(int argc, const char *argv[]) {
     argparse::ArgumentParser args{"YT Downloader"};
+    args.add_argument("-u", "--url")
+        .help("specifies video URL");
+
     try {
         args.parse_args(argc, argv);
+        actions(std::move(args));
     } catch (const std::runtime_error& err) {
         std::cerr << err.what() << std::endl;
+        return 1;
     } catch (...) {
         std::cerr << "ERROR" << std::endl;
+        return 1;
     }
     return 0;
 }
