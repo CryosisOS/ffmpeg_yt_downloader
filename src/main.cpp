e63294a5d020a3f7990b6058f0dfed8965f6b18b
PotatoMaster101 <PotatoMaster101@users.noreply.github.com>
2020-08-24 20:58:12 +0800

    Add CSV code

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,15 @@
 #include <iostream>
 #include <utility>
 #include "include/argparse.hpp"
+#include "include/csv.hpp"
 
+// Argument actions.
 void actions(argparse::ArgumentParser&& args) {
     if (auto url = args.present("-u")) {
         std::cout << (std::string("URL: ") + url.value()) << std::endl;
+    }
+    if (auto csvfile = args.present("-c")) {
+        std::cout << (std::string("CSV: ") + csvfile.value()) << std::endl;
     }
 }
 
@@ -18,6 +23,8 @@ int main(int argc, const char *argv[]) {
     argparse::ArgumentParser args{"YT Downloader"};
     args.add_argument("-u", "--url")
         .help("specifies video URL");
+    args.add_argument("-c", "--csv")
+        .help("specifies a CSV file to read in");
 
     try {
         args.parse_args(argc, argv);
